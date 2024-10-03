0c55c74145bb22cf3eea712ce1a05087ceb6311f
Muhamed <muhamedgedgafov18@gmail.com>
2024-10-03 13:55:20 +0300

    Add files via upload

---
+++ mobule_1_4.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+spent_hours = 1.5
+course_name = 'Python,'
+time_per_task = spent_hours / completed_tasks
+print('Курс:', course_name, 'всего задач:', completed_tasks,', затрачено часов:', spent_hours,', среднее время выполнения', time_per_task, 'часа.')
